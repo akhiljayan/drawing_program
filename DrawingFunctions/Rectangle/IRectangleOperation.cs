9e54d0ade07fa62d857c714c3b148cd20b054abd
AkhilJayan <mailtoakhiljayan@gmail.com>
2018-08-01 19:11:03 +0800

    Alltred rectangle

---
+++ DrawingFunctions/Rectangle/IRectangleOperation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingFunctions.Rectangle
+{
+    public interface IRectangleOperation
+    {
+    }
+}
---
+++ DrawingFunctions/Rectangle/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿using DrawingFunctions.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingFunctions.Rectangle
+{
+    public class Rectangle
+    {
+
+    }
+}
